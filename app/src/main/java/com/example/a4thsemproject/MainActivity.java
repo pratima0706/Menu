134b156a9a932f1b2b51efed763ae759eab7bdff
Pratima Neupane <pratimaneupane061@gmail.com>
2024-07-19 18:48:12 +0545

    first commit

---
+++ app/src/main/java/com/example/a4thsemproject/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.a4thsemproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavigationView nav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        nav=findViewById(R.id.nav);
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                 int id=item.getItemId();
+                 if (id==R.id.home){
+                     Intent i1= new Intent(getApplicationContext(), MainActivity.class);
+                     startActivity(i1);
+                 }
+                if (id==R.id.About){
+                    Intent i2= new Intent(getApplicationContext(), About.class);
+                    startActivity(i2);
+                }
+                if (id==R.id.Contact){
+                    Intent i3= new Intent(getApplicationContext(), Contact.class);
+                    startActivity(i3);
+                }
+                if (id==R.id.Setting){
+                    Intent i4= new Intent(getApplicationContext(), Setting.class);
+                    startActivity(i4);
+                }
+                return false;
+            }
+        });
+
+        final DrawerLayout drawerLayout= findViewById(R.id.main);
+        findViewById(R.id.nav).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        ImageView imageView=findViewById(R.id.img);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+    }
+}
